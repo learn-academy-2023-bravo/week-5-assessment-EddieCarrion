20ea72ee907fb6547f4f834eedcdc38a8bc47176
EddieCarrion.github.io <ecar25L@gmail.com>
2023-04-16 19:12:34 -0700

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,39 +14,184 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
+// describe ("codedMessage", () => {
+//   it ("will take in a string as a parameter and return a string with a coded message", () => {
+//       expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//       expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//       expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+      
+//   })
+// })
+ //Good Failure
+ // Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.166 s
+
+// const secretCodeWord1 = "Lackadaisical"
+// // Expected output: "L4ck4d41s1c4l"
+// const secretCodeWord2 = "Gobbledygook"
+// // Expected output: "G0bbl3dyg00k"
+// const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+
+// Process
+// Create a funtion called "codedMessage" that takes in a string as a parameter
+// Convert the string parameter to lowercase and store it into a new varible "string"
+// Create an empty string that will store the coded message
+// Use a for loop that will analyze each character in the string
+// Create a conditional statement that will check if each character is "a,e,i,o", if so then replace them with 4310
+// return the coded message using the "finalMessage" variable
+
+// const codedMessage = (strings) => {
+//     let string = strings.toLowerCase()
+//     let finalMessage = ""
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i] === "a") {
+//             finalMessage += "4"
+//         } else if (string[i] === "e") {
+//             finalMessage += "3"
+//         } else if (string[i] === "i") {
+//             finalMessage += "1"
+//         } else if (string[i] === "o") {
+//             finalMessage += "0"
+//         } else {
+//             finalMessage += strings[i]
+//         }
+//    }
+//    return finalMessage
+
+// }
+
+// Test Passed
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.134 s, estimated 1 s
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+// describe ("words", () => {
+//     it ("will take in an array and a single letter as a parameter and return an array of all the words with that particular letter", () => {
+//         expect(words(fruitArray,letterA)).toEqual(["Mango", "Apricot", "Peach"])
+//         expect(words(fruitArray,letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+        
+        
+//     })
+//   })
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
+//   // Good Failure
+//   // Test Suites: 1 failed, 1 total
+// // Tests:       1 failed, 1 total
+// // Snapshots:   0 total
+// // Time:        0.17 s, estimated 1 s
+
+// const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+
+// const letterA = "a"
+// // Expected output: ["Mango", "Apricot", "Peach"]
+// const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+
+// Process
+// Create a function called "words" that takes in an array and a letter as parameters
+// Create an empty array that will store our final outcome
+// Create a for loop that will loop through the array 
+// Convert the letter parameter to lower case using the ".toLowerCase()" method
+// Convert the current word to lower case
+// Create a conditional statement that checks for the index of the letter in the word
+// If the word contains the letter then it will be pushed into the new array
+// return "finalArray"
+
+
+// const words = (array1,letter1) => {
+//     let finalArray = [] 
+//     for (let i = 0; i<array1.length; i++) {
+//         let letter2 = letter1.toLowerCase()
+//         let word = array1[i].toLowerCase()
+//         if (word.indexOf(letter2)!== -1) {
+//             finalArray.push(array1[i])
+//         }
+//     } return finalArray
+// }
+
+// Test Passed
+//Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.157 s, estimated 1 s
+// Ran all test suites.
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
+// describe ("fullHouse", () => {
+//     it ("will take in an array of 5 numbers as a parameter and determines if the array is a fullhouse", () => {
+//         expect(fullHouse(hand1)).toEqual(true)
+//         expect(fullHouse(hand2)).toEqual(false)
+//         expect(fullHouse(hand3)).toEqual(false)
+//         expect(fullHouse(hand4)).toEqual(true)
+        
+        
+//     })
+//   })
+
+
+// // Test Suites: 1 failed, 1 total
+// // Tests:       1 failed, 1 total
+// // Snapshots:   0 total
+// // Time:        0.174 s, estimated 1 s
+// // Ran all test suites.
+
+
+// const hand1 = [5, 5, 5, 3, 3]
+// // Expected output: true
+// const hand2 = [5, 5, 3, 3, 4]
+// // Expected output: false
+// const hand3 = [5, 5, 5, 5, 4]
+// // Expected output: false
+// const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
+// Process
+// Create a function called fullHouse that takes in an array of numbers as a parameter
+// Sort those numbers from least to greatest
+// Create a conditional statement that will check if the first two numbers are the same
+// Within the same conditional statment check if the last three numbers are the same
+// If both conditonal statments are true then return true
+// Create another conditional statement that will check if the first three numbers are the same
+// Within the same conditional statment check if the last two numbers are the same
+// If both conditonal statments are true then return true
+// Otherwise return false
+
+// const fullHouse = (num) => {
+//     num.sort()
+//     if (num[0]=== num[1]&&num[1]!== num[2]){
+//         if (num[2]=== num[3]&&num[3]=== num[4]) {
+//             return true
+//         } 
+//     } 
+//     if (num[0]=== num[1]&&num[1]=== num[2]){
+//         if (num[2]!== num[3]&&num[3]=== num[4]) {
+//             return true
+//         } 
+//     }
+//     return false
+// }
+
+// test passed
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.164 s, estimated 1 s
+// Ran all test suites.
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: Calling the super method allows information to be transferred to classes and sub classes
 
-Researched answer:
+Researched answer: Calling the super method will send a specific message that allows a subclass to borrow specific data from the superclass. For example you can have a superclass called "Animal" with specific information and then extract that information in a subclass called "Four legged".
 
 2. What is a gem?
 
-Your answer:
+Your answer:A gem is a nest that holds snippets of code.
 
-Researched answer:
+Researched answer: A gem is a library that contains snippets of coding data. Ruby manages these snippets of code in a manger called RubyGems. This makes it much easier for a web developer to share code with other web developers.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a database that contains information in rows and columns. I am not quite sure if there are other types of databases but I would assume they exist and are not meant to be shared.
 
-Researched answer:
+Researched answer: A relational database is a database that can be shared with others and resembles a spreadsheet because it contains columns and rows. They are also no relational databases often called "NoSQL" databases. They differ from relational databases in that they contain information differently (not in a table). A document would be an example of what a non-relational datatype structure would look like. A document contains alot of information that contains specific details, making it more flexible than a relational database. 
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Primary keys is a specific identifier for a specific class instance. Primary keys are important because we can use them to access specific information as needed.
 
-Researched answer:
+Researched answer: Primary keys contain unique unique values and can identify each row in a table. They are important because a database needs them to update,delete, and insert information to the table. They also prevent issues since they contain unique id's for each instance.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer:I do not remember off of the top of my head. 
 
-Researched answer:
+Researched answer:The HTTP verbs are POST, GET, PUT, PATCH, and DELETE. They relate to the acronym CRUD because they essentially create, read, update, and delete. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: Model validations ensure that only the valid information is stored in the database. Rails rus all model validations before information is added and will not save if the validations experiences any errors.
 
-2. RESTful routes:
+2. RESTful routes:Routes are how specific requests are directed to code that handles the routes. RESTful routes is a more structured way of defining how a route will work. The yprovide mapping between HTTP verbs.
 
-3. ERB:
+3. ERB:ERB stand for Embedded Ruby, it is a file that allows the web developer to combine HTML and Ruby code.
 
-4. Params:
+4. Params:Parms allow us to add information by passing a value. Adding Params allows the developer to add information by passing the value into the route rather than the URL.
 
-5. API:
+5. API:API (Application Programming Interface) which is a set of protocols for building application software.
